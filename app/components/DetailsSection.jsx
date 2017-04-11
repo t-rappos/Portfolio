cd3394d1499204d83af54e120010d8e382ee315d
t_rappos <t_rappos@hotmail.com>
2017-04-11 19:48:22 +1000

    added markdown functionality and updated 'in details' copy

--- app/components/DetailsSection.jsx
+++ app/components/DetailsSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {  MarkdownPreview  } from 'react-marked-markdown';
 let headings = require('./headings.jsx');
 
 const detailsSectionStyle = {
@@ -9,7 +10,7 @@ const detailsSectionStyle = {
 };
 
 const detailsTextStyle = {
-  width: '400px',
+  width: '500px',
   marginLeft : 'auto',
   marginRight : 'auto',
   overflow: 'hidden',
@@ -25,70 +26,85 @@ class DetailsSection extends React.Component {
     super(props);
   }
   render() {
+
     return (
     <div style = {detailsSectionStyle}>
       <h2 id='in-detail' style = {headings.headingStyle}>In Detail</h2>
       <div style = {detailsTextStyle}>
-
         <p>I'm currently studying a double degree, Bachelor of Engineering (Robotics & Mechatronics)
           (Honours)/ Bachelor of Computer Science at Swinburne University of Technology.
            I've always had a fascination about how things work and a drive to understand systems
-            and the interaction between mechanics, robotics, electronics and software.</p>
+            especially the interaction between mechanics, robotics, electronics and software.</p>
 
-          <p>The following are subjects that I'm interested in and have had experience with.</p>
+        <p>The following are subjects that I'm interested in and have had experience with.</p>
+        <MarkdownPreview value="
+### Front End
+- Javascript (ES6)
+- React
+- Redux
+- Socket IO
+- Webpack
+- CSS (Foundation, Bootstrap, SCSS)
+- HTML5
+"/>
 
-        Front End
-Javascript (ES6)
-React
-Redux
-Socket IO
-Webpack
-CSS (Foundation, Bootstrap, SCSS)
-HTML5
+<MarkdownPreview value="
+### Back End
+- Javascript (NodeJS, Express)
+- Java (Spring)
+- SQL (MySQL, PostgreSQL)
+- NPM
+"/>
 
-Back End
-Javascript (NodeJS, Express)
-Java (Spring)
-SQL (MySQL, PostgreSQL)
-NPM
+<MarkdownPreview value="
+### Software Applications
+- Python (SciPy, RenPy, NumPy)
+- CMake (Build configuration)
+- Git (GitHub, BitBucket, GitLab)
+- Trac
+- C++ (STL, VTK, QT, ParaView, OpenGL, OpenCV)
+- C#
+- Java (Android Studio, Gradle)
+"/>
 
-Software Applications
-Python (SciPy, RenPy, NumPy)
-CMake (Build configuration)
-Git (GitHub, BitBucket, GitLab)
-Trac
-C++ (STL, VTK, QT, ParaView, OpenGL, OpenCV)
-C#
-Java (Android Studio, Gradle)
+<MarkdownPreview value="
+### Embedded Devices
+- C (CodeWarrior, Arduino IDE, HCS12 uController)
+- C++ (Raspberry PI)
+- ROS (Robotic Operating System)
+"/>
 
-Embedded Devices
-C (CodeWarrior, Arduino IDE, HCS12 uController)
-C++ (Raspberry PI)
-ROS (Robotic Operating System)
+<MarkdownPreview value="
+### Continuous Deployment and Integration
+- Hosting
+  - Amazon EC2 AWS (WordPress)
+  - Heroku (Automated Deployment)
+"/>
 
-Continuous Deployment and Integration
-Hosting
-Amazon EC2 AWS (WordPress)
-Heroku (Automated Deployment)
+<MarkdownPreview value="
+### Integration / Deployment
+- TravisCI (Automated testing and Deployment)
+"/>
 
-Integration / Deployment
-TravisCI (Automated testing and Deployment)
+<MarkdownPreview value="
+### Testing
+- Testing (Mocha)
+- Linting (ESLint)
+"/>
 
-Testing
-Testing (Mocha)
-Linting (ESLint)
+<MarkdownPreview value="
+### Workflow
+"/>
 
-
-My workflow
-
-	<p>My current workflow is guided by test driven development, supported by unit tests,
-     continuous integration and deployment. Following a document as you go (but before development),
-      traceable agile approach. I aim to work as transparently as possible by using tools such as Trello,
-               and storing working research in visible locations (dropbox, git, google docs).
-                The ultimate goal is to be able to stop working on a project and pick it up months later
-                 and be able to start working straight away.</p>
-                <a href = 'https://github.com/t-rappos/auction-system/blob/master/doc/workflow.md'>See github for a more in-depth explanation</a>
-
+<p>My current workflow is guided by test driven development, supported by unit tests,
+continuous integration and automated deployment. Following a document-as-you-go (but before development),
+traceable agile approach. I aim to work as transparently as possible by using tools such as Trello,
+and storing working research in visible locations (dropbox, git, google docs).
+The ultimate goal is to be able to stop working on a project and pick it up months later
+and be able to start working straight away.</p>
+        <a href = 'https://github.com/t-rappos/auction-system/blob/master/doc/workflow.md'>See github for a more in-depth explanation</a>
+<br/>
+<br/>
       </div>
     </div>  );
   }
